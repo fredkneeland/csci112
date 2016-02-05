cdc0a33950b2b18f3a77e17c21df5288066ca38f
Fred Kneeland <fred@Freds-iMac.local>
2016-02-04 23:17:42 -0700

    working on things

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,4 +1,5 @@
 #import <stdio.h>
+#import <math.h>
 
 //////////////////////////////////////////////////////////////////////////////////
 //										//
@@ -8,8 +9,17 @@
 
 char WRONG_INPUT_LENGTH_MSG[] = "Array Lengths must be greater than or equal to 0\n";
 char GET_ARRAY_LENGTH_MSG[] = "Please enter the length of the array\n";
-char GET_VALUE_FROM_USER[] = "Please enter the next value from the array";
+char GET_VALUE_FROM_USER[] = "Please enter the next value from the array ";
 char WRONG_VALUE_MSG[] = "Values must be positive";
+char FINAL_RANGE_PRINT_MSG[] = "Range: ";
+char END_OF_LINE_CHAR[] = "\n";
+char FINAL_AVERAGE_PRINT_MSG[] = "Average: ";
+char FINAL_HIGHEST_VALUE_PRINT_MSG[] = "Highest Value: ";
+char FINAL_LOWEST_VALUE_PRINT_MSG[] = "Lowest Value: ";
+char FINAL_STD_DEV_PRINT_MSG[] = "Standard Deviation: ";
+char FINAL_SUM_PRINT_MSG[] = "Sum: ";
+char FINAL_SUM_SQUARES_MSG[] = "Squared Sum: ";
+
 int MAX_INT_VALUE = 2147483647;
 
 
@@ -24,14 +34,14 @@ int MAX_INT_VALUE = 2147483647;
  * This function will return a value received from the user
  * after printint out a msg passed in as a parameter
  */
-int getValueFromUser(char[] msg_to_be_printed)
+int getValueFromUser(char msg_to_be_printed[])
 {
 	// initialize our input variable
 	int value_from_user = 0;
 	// print out prompt for user
-	printf(msg_to_be_printed);
+	printf("%s", msg_to_be_printed);
 	// scan in value from user
-	scanf("%d", &value);
+	scanf("%d", &value_from_user);
 	// return the value given by user to whoever called us
 	return value_from_user;
 }
@@ -39,7 +49,7 @@ int getValueFromUser(char[] msg_to_be_printed)
 /**
  * This function gets a value from the user between the min and max ranges
  */
-int getValueFromUserInRange(char[] msg_to_be_printed, char[] msg_to_be_printed_in_case_of_error, int minValue, int maxValue)
+int getValueFromUserInRange(char msg_to_be_printed[], char msg_to_be_printed_in_case_of_error[], int minValue, int maxValue)
 {
 	// initialize our variable
 	int user_given_value;
@@ -51,7 +61,7 @@ int getValueFromUserInRange(char[] msg_to_be_printed, char[] msg_to_be_printed_i
 	while (user_given_value < minValue || user_given_value > maxValue)
 	{
 		// print error msg
-		printf(msg_to_be_printed_in_case_of_error);
+		printf("%s", msg_to_be_printed_in_case_of_error);
 		// ask the user for the value again
 		user_given_value = getValueFromUser(msg_to_be_printed);
 	}
@@ -63,7 +73,7 @@ int getValueFromUserInRange(char[] msg_to_be_printed, char[] msg_to_be_printed_i
 /**
  * This is the main method that will execute when we run this file
  */
-int main 
+int main() 
 {
 	// get array length from user between the values of 0 and MAX_INT_VALUE
 	int user_given_array_length = getValueFromUserInRange(GET_ARRAY_LENGTH_MSG, WRONG_INPUT_LENGTH_MSG, 0, MAX_INT_VALUE);
@@ -72,11 +82,14 @@ int main
 	int array_of_user_values[user_given_array_length];
 	
 	// initialize result values
-	int lowest = 0,
-	    highest = MAX_INT_VALUE, 
-	    avg = 0, 
+	int lowest = MAX_INT_VALUE,
+	    highest = 0,
+	    sum = 0, 
 	    sum_squares = 0, 
-	    std_dev, range;
+	    range;
+
+	double std_dev,
+	       average = 0;
 
 	// initialize our loop counter
 	int loop_counter;
@@ -87,12 +100,78 @@ int main
 		// set the next value in the array to a user given value
 		array_of_user_values[loop_counter] = getValueFromUserInRange(GET_VALUE_FROM_USER, WRONG_VALUE_MSG, 0, MAX_INT_VALUE);
 		
-		// check for lowest
+		// check if current value is lower than lowest value
 		if (array_of_user_values[loop_counter] < lowest)
 		{
-			lowest = array_pf_user_values[loop_counter];
+			// update lowest
+			lowest = array_of_user_values[loop_counter];
 		}
+		// check if current value is greater than current highest
+		else if (array_of_user_values[loop_counter] > highest)
+		{
+			// update highest
+			highest = array_of_user_values[loop_counter];
+		}
+
+		// update the total sum
+		sum += array_of_user_values[loop_counter];
+		// update the total sum squared
+		sum_squares += (array_of_user_values[loop_counter]) * (array_of_user_values[loop_counter]);
 	}
+
+	// set range
+	range = highest - lowest;
+
+	// check if length == 0 to prevent a division by 0 error
+	if (user_given_array_length == 0)
+	{
+		average = 0;
+		std_dev = 0;
+	}
+	else
+	{
+		// calculate average
+		average = (double) sum / (double) user_given_array_length;
+		// calculate standard deviation
+		std_dev = sqrt((sum_squares / user_given_array_length) - (average * average));
+	}
+
+	///////////// Print out all of the values \\\\\\\\\\\\\\\\\\\\
+
+	// print out the average value
+	printf("%s", FINAL_AVERAGE_PRINT_MSG);
+	printf("%f", average);
+	printf("%s", END_OF_LINE_CHAR);
+
+	// print out the range
+	printf("%s", FINAL_RANGE_PRINT_MSG);
+	printf("%d", range);
+        printf("%s", END_OF_LINE_CHAR);
+
+	// print out the highest value
+	printf("%s", FINAL_HIGHEST_VALUE_PRINT_MSG);
+	printf("%d", highest);
+        printf("%s", END_OF_LINE_CHAR);
+
+	// print out the lowest value
+	printf("%s", FINAL_LOWEST_VALUE_PRINT_MSG);
+	printf("%d", lowest);
+        printf("%s", END_OF_LINE_CHAR);
+
+	// print out the sum
+	printf("%s", FINAL_SUM_PRINT_MSG);
+	printf("%d", sum);
+        printf("%s", END_OF_LINE_CHAR);
+
+	// print out the sum of the squares
+	printf("%s", FINAL_SUM_SQUARES_MSG);
+	printf("%d", sum_squares);
+        printf("%s", END_OF_LINE_CHAR);
+
+	// print out the standard deviation
+	printf("%s", FINAL_STD_DEV_PRINT_MSG);
+	printf("%f", std_dev);
+        printf("%s", END_OF_LINE_CHAR);
 
 	// exit normally
 	return 0;
